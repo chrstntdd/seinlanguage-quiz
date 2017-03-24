a2ccf9982e9d0d10dda307b2f33bfcbd724a65c5
Christian Todd <christiantodd27@icloud.com>
2017-03-23 22:32:03 -0400

    begin adding JS.

--- app.js
+++ app.js
@@ -1,1 +1,136 @@
-document.write('no');
+$(function () {
+  //
+});
+
+var state = {
+  questions: [
+    {
+      question: 'What did Jerry accidentally call Michael Richard\'s character in the pilot episode?',
+      answers: ['Kramer', 'Keagan', 'Kessler', 'Ketchup'],
+      correctAnswerIndex: 2
+    },
+    {
+      question: 'How many seasons of Seinfeld are there?',
+      answers: [10,6,12,9],
+      correctAnswerIndex: 3
+    },
+    {
+      question: 'What is the name of the Fried Chicken chain that opens up across the street from Kramer?',
+      answers: ['Tyler\'s Chicken', 
+                'Kentucky Fried Cicken',
+                'Brendan\'s Southern Style Poultry',
+                'Popeye\'s'],
+      correctAnswerIndex: 0
+    },
+    {
+      question: 'Why did George give up his Gore-Tex jacket to the liqor store clerk?',
+      answers: ['George was hypnotized by the clerk',
+                'George destroyed some merchandise and had no cash to reimburse the store', 
+                'George felt bad for the clerk since there was no heat in the building',
+                'George lost a bet'],
+      correctAnswerIndex: 1
+    },
+    {
+      question: 'What is George\'s debit card code?',
+      answers: ['JERRY',
+                'BOSCO',
+                'BONOS',
+                'COSMO'],
+      correctAnswerIndex: 1
+    }
+  ],
+  currentQuestionIndex: 0,
+  correctCount: 0,
+  route: 'start',  //options: 'start','question','final-feedback'
+  prevAnswerCorrect: false
+
+};
+
+//MANAGE STATE
+
+function setRoute(state, route) {
+  state.route = route;
+}
+
+function resetQuiz(state) {
+  state.correctCount = 0
+  state.currentQuestionIndex = 0;
+  setRoute(state, 'start');
+}
+
+function nextQuestion(state) {
+  var questionNum = state.currentQuestionIndex;
+  questionNum++;
+  questionNum === state.questions.length ?
+    setRoute(state, 'final-feedback') :
+    setRoute(state, 'question');
+}
+
+
+//DOM MANIPULATION (RENDERS)
+
+function renderQuestionCard(state, element) {
+  renderQuestionText(state, element.find('.js-question-text'));
+  renderQuestionCount(state.element.find('.js-question-count'));
+  renderChoices(state, element.find('.answer'));
+  if (currentQuestionIndex > 0) {
+    renderAnswerFeedback(state, element.find('.js-answer-feedback'));
+  }
+}
+
+function renderQuestionText(state, element) {
+  var currentQuestion = state.questions[state.currentQuestionIndex];
+  element.text(currentQuestion.question);
+}
+
+function renderQuestionCount(state, element) {
+  var questionCount = (state.currentQuestionIndex + 1) + '/' + state.questions.length;
+  element.html(questionCount);
+}
+
+function renderQuestionChoices(state, element) {
+  var currentQuestion = state.questions[state.currentQuestionIndex];
+  var questionChoices = currentQuestion.answers.map((answer, index) =>
+    (
+      '<label class="answer" ' +
+      '<input type="radio" name="answerChoice" value="' + index + '">' +
+      answer +
+      '</label>'
+    )
+  );
+  element.html(questionChoices);
+}
+
+function renderAnswerFeedback(state, element) {
+  var feedbackHTML = state.prevAnswerCorrect ?
+    '<h4>That\'s right!</h4>' :
+    '<h4>Sorry, that\'s not right.</h4>';
+  element.html(feedbackHTML);
+}
+
+function renderFinalFeedback(state, element) {
+  var feedbackHTML = state.score + ' out of ' + state.questions.length + ' questions right.';
+  element.html(feedbackHTML);
+}
+
+//EVENT HANDLERS
+
+function handleStartQuiz() {
+  /*
+  After start button is pressed, load first question card.
+  */
+}
+
+function handleAnswerSubmits() {
+  /*
+  When the submit answer button is pressed, check answer, update state score object, 
+  increment currentQuestionIndex, render feedback response.
+  */
+}
+
+function handleEndQuiz() {
+  /*
+  Once the currentQuestion is equal to the length of questions array,
+  run renderFinalFeedback, present start over button.
+  */
+}
