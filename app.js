346640d95c0a6fb5f5c1e5222c650b93f8164647
Christian Todd <christiantodd27@icloud.com>
2017-03-25 13:52:55 -0400

    add inital functions to get desired functionality.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(function () {
   handleAnswerFeedback();
+  handleAnswerSubmits();
+  beginQuiz();
+  renderQuestionCard();
 });
 
 var state = {
@@ -7,12 +10,12 @@ var state = {
     {
       question: 'What did Jerry accidentally call Michael Richard\'s character in the pilot episode?',
       answers: ['Kramer', 'Keagan', 'Kessler', 'Ketchup'],
-      correctAnswerIndex: 2
+      correctAnswer: 'Kessler'
     },
     {
       question: 'How many seasons of Seinfeld are there?',
       answers: [10,6,12,9],
-      correctAnswerIndex: 3
+      correctAnswer: 9 
     },
     {
       question: 'What is the name of the Fried Chicken chain that opens up across the street from Kramer?',
@@ -20,7 +23,7 @@ var state = {
                 'Kentucky Fried Cicken',
                 'Brendan\'s Southern Style Poultry',
                 'Popeye\'s'],
-      correctAnswerIndex: 0
+      correctAnswer: 'Tyler\'s Chicken'
     },
     {
       question: 'Why did George give up his Gore-Tex jacket to the liqor store clerk?',
@@ -28,7 +31,7 @@ var state = {
                 'George destroyed some merchandise and had no cash to reimburse the store', 
                 'George felt bad for the clerk since there was no heat in the building',
                 'George lost a bet'],
-      correctAnswerIndex: 1
+      correctAnswer: 'George destroyed some merchandise and had no cash to reimburse the store'
     },
     {
       question: 'What is George\'s debit card code?',
@@ -36,108 +39,80 @@ var state = {
                 'BOSCO',
                 'BOBOS',
                 'COSMO'],
-      correctAnswerIndex: 1
+      correctAnswer: 'BOSCO'
     }
   ],
   currentQuestionIndex: 0,
   correctCount: 0,
-  route: 'start',  //options: 'start','question','final-feedback'
   prevAnswerCorrect: false
 
 };
 
 //MANAGE STATE
 
-function setRoute(state, route) {
-  state.route = route;
-}
 
 function resetQuiz(state) {
   state.correctCount = 0
   state.currentQuestionIndex = 0;
-  setRoute(state, 'start');
 }
 
-function nextQuestion(state) {
-  var questionNum = state.currentQuestionIndex;
-  questionNum++;
-  questionNum === state.questions.length ?
-    setRoute(state, 'final-feedback') :
-    setRoute(state, 'question');
+function renderQuestionCard(){
+  var currentQuestionObj = state.questions[state.currentQuestionIndex];
+  renderQuestionPrompt();
+  renderQuestionChoices(currentQuestionObj.answers);
 }
 
+function renderQuestionPrompt() {
+  var progressHTML = '<span>(' + (state.currentQuestionIndex + 1) + '/' + state.questions.length + ')</span>'
+  var questionText =  state.questions[state.currentQuestionIndex].question;
+  $('.js-question-text').html(progressHTML + questionText);
+}
 
-//DOM MANIPULATION (RENDERS)
+function renderQuestionChoices(answers){ //array
+  $('#question-form label').each(function(index, label){
+    $(this).find('input').attr('value', answers[index]);
+    $(this).find('span').text(answers[index]);
+  });
+}
 
-function renderQuestionCard(state, element) {
-  renderQuestionText(state, element.find('.js-question-text'));
-  renderQuestionCount(state.element.find('.js-question-count'));
-  renderChoices(state, element.find('.answer'));
-  if (currentQuestionIndex > 0) {
-    renderAnswerFeedback(state, element.find('.js-answer-feedback'));
+function checkAnswer(userChoice){
+  var correctChoice = state.questions[state.currentQuestionIndex].correctAnswer;
+  if(userChoice === correctChoice){
+    alert('RIGHT');
+  } else {
+    alert('wrong');
   }
 }
 
-function renderQuestionText(state, element) {
-  var currentQuestion = state.questions[state.currentQuestionIndex];
-  element.text(currentQuestion.question);
-}
+//DOM MANIPULATION (RENDERS)
 
-function renderQuestionCount(state, element) {
-  var questionCount = (state.currentQuestionIndex + 1) + '/' + state.questions.length;
-  element.html(questionCount);
-}
-
-function renderQuestionChoices(state, element) {
-  var currentQuestion = state.questions[state.currentQuestionIndex];
-  var questionChoices = currentQuestion.answers.map((answer, index) =>
-    (
-      '<label class="answer" ' +
-      '<input type="radio" name="answerChoice" value="' + index + '">' +
-      answer +
-      '</label>'
-    )
-  );
-  element.html(questionChoices);
-}
-
-
-function renderFinalFeedback(state, element) {
-  var feedbackHTML = state.score + ' out of ' + state.questions.length + ' questions right.';
-  element.html(feedbackHTML);
+function beginQuiz(){
+  $('#start-quiz').click(function(e){
+    $('#my-quiz').removeClass('hidden');
+    $('#start-quiz').addClass('hidden');
+  });
 }
 
 //EVENT HANDLERS
 
-function handleStartQuiz() {
-  /*
-  After start button is pressed, load first question card.
-  */
-}
-
 function handleAnswerSubmits() {
-  /*
-  When the submit answer button is pressed, check answer, update state score object, 
-  increment currentQuestionIndex, render feedback response, load next question.
-  */
+  $('#submit-answer').click(function(e){
+    e.preventDefault();
+    var userChoice = $('input[name="answerChoice"]:checked').val();
+    checkAnswer(userChoice);
+  });
 }
 
-function handleEndQuiz() {
-  /*
-  Once the currentQuestion is equal to the length of questions array,
-  run renderFinalFeedback, present start over button.
-  */
-}
 
 function handleAnswerFeedback() {
   //OPEN MODAL
-  $('[data-popup-open]').on('click', function (e) {
+  $('#submit-answer').on('click', function (e) {
     var targetPopupClass = $(this).attr('data-popup-open');
     $('[data-popup="' + targetPopupClass + '"]').fadeIn(350);
     e.preventDefault();
   });
   //CLOSE MODAL
-  $('[data-popup-close]').on('click', function (e) {
+  $('#close-feedback-modal').on('click', function (e) {
     var targetPopupClass = $(this).attr('data-popup-close');
     $('[data-popup="' + targetPopupClass + '"]').fadeOut(350);
     e.preventDefault();
